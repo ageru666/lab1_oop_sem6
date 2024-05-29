8f62c447a37973855473d2c54dce66230ce7ee79
ageru666 <ageru2003@gmail.com>
2024-05-30 00:09:29 +0300

    deleted unnecessary folder

--- CarRentalBack/src/main/webapp/Data/Models/CarAvailable.java
+++
@@ -1,9 +0,0 @@
-package main.webapp.Data.Models;
-
-import lombok.Data;
-
-@Data
-public class CarAvailable {
-    private int id;
-    private String status;
-}
--- CarRentalBack/src/main/webapp/Data/Models/Login.java
+++
@@ -1,9 +0,0 @@
-package main.webapp.Data.Models;
-
-import lombok.Data;
-
-@Data
-public class Login {
-    private String username;
-    private String password;
-}
